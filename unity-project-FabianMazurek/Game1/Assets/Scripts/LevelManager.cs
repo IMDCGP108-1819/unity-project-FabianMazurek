12284fbf8c65b6d088807f16c914931ea2a6c055
FabianMazurek <fabianmazurek2000@gmail.com>
2018-11-18 21:00:31 +0000

    Extended the first level, added new double jump mechanic and tips in-game

---
+++ unity-project-FabianMazurek/Game1/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckpoint;
+    public GameObject trampoline;
+    private PlayerMovement player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerMovement>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player Respawn");
+        player.transform.position = currentCheckpoint.transform.position;
+        
+    }
+}
