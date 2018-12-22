de112365df25c81070ef76cd69afd91330e44b8b
FabianMazurek <fabianmazurek2000@gmail.com>
2018-12-22 00:10:13 +0000

    Added the main menu and the base layout for the sub menus

---
+++ unity-project-FabianMazurek-master/Game1/Assets/QuitButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class QuitButton : MonoBehaviour
+{
+    public void doExitGame()
+    {
+        Application.Quit();
+        Debug.Log("game is exiting");
+    }
+}
---
+++ unity-project-FabianMazurek-master/Game1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float m_Speed;
+    public float m_Speed2;
+    public float jumpHeight;
+    public float velocity;
+    
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+    public float Speed = 0;
+    public float MaxSpeed = 10;
+    public float Acceleration = 2.5f;
+    public float Deceleration = 3.5f;
+    
+   
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Code that checks if the player is on the ground.
+        
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        
+    }
+    void Update()
+    {
+        //player movement
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-m_Speed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(m_Speed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+      
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+
+         
+            
+        }
+      
+    }
+}
